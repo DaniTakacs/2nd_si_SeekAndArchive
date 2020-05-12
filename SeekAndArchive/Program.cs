8b142fb5599cac2a29e674f53103e93dfa580cad
DaniTakacs <takacs.d24@gmail.com>
2020-05-12 18:27:19 +0200

    Add project files.

---
+++ SeekAndArchive/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace SeekAndArchive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo di = new DirectoryInfo(args[1]);
+            String fileToFind = args[0];
+
+            Console.WriteLine("This is the thing to be printed " + di.GetFiles(fileToFind)[0]);
+        }
+    }
+}
