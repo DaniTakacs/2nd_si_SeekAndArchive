d16915bbeed8b0e7af05115aff396008154d2e52
DaniTakacs <takacs.d24@gmail.com>
2020-05-26 19:23:10 +0200

    Assignment finished

--- SeekAndArchive/Program.cs
+++ SeekAndArchive/Program.cs
@@ -8,10 +8,35 @@ namespace SeekAndArchive
     {
         static void Main(string[] args)
         {
-            DirectoryInfo di = new DirectoryInfo(args[1]);
-            String fileToFind = args[0];
+            string fileToFind =args[0];
+            string root = args[1];
 
-            Console.WriteLine("This is the thing to be printed " + di.GetFiles(fileToFind)[0]);
+            string[] subdirectoriesEntries = Directory.GetDirectories(root);
+
+            foreach (var subdierctory in subdirectoriesEntries)
+            {
+                LoadSubDirs(subdierctory, fileToFind);
+            }
+        }
+
+        private static void LoadSubDirs(String dir, string filename)
+        {
+
+            var matchingFiles = Directory.GetFiles(dir, filename);
+
+            if (matchingFiles.Length > 0)
+            {
+                foreach (var matchingFile in matchingFiles)
+                {
+                    Console.WriteLine(matchingFile.ToString());
+                }
+            }
+
+            string[] subdirectoriesEntries = Directory.GetDirectories(dir);
+            foreach (var subdirectory in subdirectoriesEntries)
+            {
+                LoadSubDirs(subdirectory, filename);
+            }
         }
     }
 }
